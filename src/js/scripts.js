0b3c95447dce501422304b02c41fd54d21a689f0
Fanny Lemaire <flemr@Fannys-MacBook-Pro.local>
2017-09-18 14:50:46 -0400

    quick first version pushed online to test

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -3,7 +3,32 @@
   'use strict';
 
   $(function () {
-    // FastShell
+
+    var slideIndex = 0;
+    slideThisBaby();
+
+    function slideThisBaby() {
+
+      var slides = document.querySelectorAll('.slideshow__img');
+      var aSlides = Array.apply(null, slides);
+      var totalSlides = aSlides.length;
+
+      for (var i = 0; i < totalSlides; i++) {
+        aSlides[i].style.display = 'none';
+      }
+      slideIndex++;
+      if (slideIndex > totalSlides) {
+        slideIndex = 1
+      }
+       aSlides[slideIndex-1].style.display = 'block';
+      //  setTimeout(slideThisBaby, 400); // Change image every 0,4 seconds
+
+
+
+    }
+
+
+
   });
 
 })(jQuery, window, document);
